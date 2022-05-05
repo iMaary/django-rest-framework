b8b564be2a786106efb446d1f4610a34862f03c9
iMaary <m.rodriguesnave@gmail.com>
2022-05-05 12:48:05 -0300

    creating admin classes to make group of courses and students - adding a superuser to access admin page [command: python manage.py createsupersuser]

--- school/admin.py
+++ school/admin.py
@@ -1,3 +1,18 @@
+from re import search
 from django.contrib import admin
+from school.models import Student, Course
 
-# Register your models here.
+class Students(admin.ModelAdmin):
+    list_display = ('id', 'name', 'rg', 'cpf', 'birth_date')
+    list_display_link = ('id', 'name')
+    search_field = ('name')
+    list_per_page = 20
+
+admin.site.register(Student, Students)
+
+class Courses(admin.ModelAdmin):
+    list_display = ('id', 'code_course', 'description')
+    list_display_link = ('id', 'code_course')
+    search_field = ('code_course')
+
+admin.site.register(Course, Courses)
